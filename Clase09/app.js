26bf6ec0e768b72ddd36e4c2fa39a19c8eceaac0
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-09-29 22:33:18 -0300

    Clase 09 - Eventos

--- Clase09/app.js
+++ Clase09/app.js
@@ -11,8 +11,8 @@ const btn2 = document.getElementById('btn2')
 // btn1.addEventListener('click', saludar)
 
 // primer forma
-btn1.addEventListener('click', () => {
-
+btn1.addEventListener('click', (e) => {
+  
     console.log('Clickeaste el BOTON 1')
 
 })
@@ -41,6 +41,7 @@ btn2.onmouseover = () => {
 const modalAbrir = document.getElementById('modal-abrir')
 const modalCerrar = document.getElementById('modal-cerrar')
 const modalContainer = document.getElementsByClassName('modal-container')[0]
+const modal = document.getElementsByClassName('modal')[0]
 
 modalAbrir.addEventListener('click', () => {
     // modalContainer.classList.add('modal-active')
@@ -52,6 +53,13 @@ modalCerrar.addEventListener('click', () => {
     modalContainer.classList.toggle('modal-active')
 })
 
+modalContainer.addEventListener('click', () => {
+    modalContainer.classList.toggle('modal-active')
+})
+
+modal.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
 // ==== random background ===
 
 const randomColor = () => {
@@ -62,7 +70,93 @@ const randomColor = () => {
     return `rgb(${a}, ${b}, ${c})`
 }
 
-document.body.addEventListener('click', () => {
-    document.body.style.backgroundColor = randomColor()
+document.body.addEventListener('click', (event) => {
+    // console.log("X:", event.x)
+    // console.log("Y:", event.y)
+
+    // console.log(event.target)
+
+    // event.target.remove()
+    // document.body.style.backgroundColor = randomColor()
 })
 
+// ============
+
+const inputNombre = document.getElementById('input1')
+const inputApellido = document.getElementById('input2')
+const inputDestino = document.getElementById('destino')
+
+const errorNombre = document.getElementById('error-nombre')
+const errorApellido = document.getElementById('error-apellido')
+// inputNombre.onkeydown = () => { console.log("Apretaste una tecla")}
+
+inputNombre.addEventListener('input', (e) => {
+    // console.log(e)
+
+    let nombre = inputNombre.value
+
+    if (nombre.length < 4) {
+        inputNombre.classList.add('input-error')
+        errorNombre.classList.add('error-show')
+    } else {
+        inputNombre.classList.remove('input-error')
+        errorNombre.classList.remove('error-show')
+    }
+})
+
+inputApellido.addEventListener('input', () => {
+    let apellido = inputApellido.value
+
+    if (apellido.length < 4) {
+        inputApellido.classList.add('input-error')
+        errorApellido.classList.add('error-show')
+    } else {
+        inputApellido.classList.remove('input-error')
+        errorApellido.classList.remove('error-show')
+    }
+})
+
+
+const destinos = []
+
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    console.log(e)
+
+    const nombre = inputNombre.value
+    const apellido = inputApellido.value
+    const destino = inputDestino.value
+
+    if (nombre.length > 3 && apellido.length > 3) {
+
+        destinos.push({
+            nombre: nombre,
+            apellido: apellido,
+            destino: destino
+        })
+
+        console.log(destinos)
+
+        alert('Destino agregado correctamente')
+
+        formulario.reset()
+
+        // inputNombre.value = ""
+        // inputApellido.value = ""
+        // inputDestino.value = "paris"
+
+        // alert(`Felicitaciones ${nombre} ${apellido}
+        // Te vas a ${destino}`)
+
+
+        // formulario.submit()
+    }
+
+})
+
+// document.getElementById('btn-submit').addEventListener('click', (e) => {
+//     e.preventDefault()
+// })
