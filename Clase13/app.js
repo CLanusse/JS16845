914ba053a926c1fa94285a8b85236afc02c8e160
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-10-14 09:36:18 -0300

    Clase 13

--- Clase13/app.js
+++ Clase13/app.js
@@ -15,11 +15,17 @@
 
 // titilar()
 
-// setInterval(() => {
+
+// setTimeout( () => {
+//     $('h1').fadeOut(1000)
+// }, 2000)
+
+// setInterval( () => {
 //     $('h1').fadeOut(500, () => {
 //         $('h1').fadeIn(500)
 //     })
-// }, 1200);
+
+// }, 1200)
 
 
 $('#show').click( () => {
@@ -43,4 +49,81 @@ $('#fadeout').click( () => {
 // })
 
 
+// ============
 
+// $('section p').css("background-color", "#444444")
+// $('section p').css("color", "white")
+// $('section p').css("font-size", "28px")
+
+$('section p').css({
+    "background-color": "#444",
+    "color": "white",
+    // "font-size": "30px",
+    "font-style": "italic"
+})
+
+$('section p').addClass('fontBig')
+
+$('section p').click( () => {
+    $('section p').toggleClass('fontBig')
+    $('section p').toggleClass('fontSmall')
+})
+
+
+// ================
+$('h1').css({
+    "font-size": "10px",
+    "opacity": 0,
+    "color": "red"
+})
+
+$('h1').animate({
+    "font-size": "40px",
+    "opacity": 1,
+    "margin-left": "50%"
+}, 5000)
+
+
+$('#square').css('background-color', 'yellow')
+$('#square').css('width', '0')
+
+
+// $('#square')
+//     .animate({
+//         "width": "100%",
+//         "height": "100vh"
+//     }, 1000)
+//     .animate({
+//         "width": "200px",
+//         "height": "200px",
+//         "border-radius": "50%"
+//     }, 500)
+//     .fadeOut(600)
+//     .delay(1000)
+//     .slideDown(1500)
+//     .animate({
+//         "border-radius": 0
+//     }, 1000)
+//     .slideUp(500)
+//     .delay(2000)
+//     .fadeIn(500)
+//     .delay(1000)
+//     .animate({
+//         "width": "600px",
+//         "height": "600px"
+//     }, 1500)
+//     .delay(3000)
+//     .fadeOut(5000)
+
+
+//  SWAL 
+
+
+$('#swal').click( () => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Funciona!',
+        text: 'Esta librería es genial'
+        }
+      )
+})
